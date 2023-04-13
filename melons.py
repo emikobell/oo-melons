4d1f9baab13b5dc795abdfe4621943637901b6c0
Emiko Bell <emikoelizabethbell@gmail.com>
2023-04-13 14:47:17 -0700

    Initial commit

---
+++ melons.py
@@ -0,0 +1,66 @@
+"""Classes for melon orders."""
+
+class AbstractMelonOrder:
+    def __init__(self,species, qty):
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+
+
+    def get_total(self):
+        """Calculate price, including tax."""
+        base_price = 5
+
+        if self.species == "Christmas":
+            base_price = base_price *1.5
+        
+        total = (1 + self.tax) * self.qty * base_price
+
+        if self.qty < 10 and self.order_type == 'international':
+            total += 3
+            
+
+        return total
+
+    def mark_shipped(self):
+        """Record the fact than an order has been shipped."""
+
+        self.shipped = True
+
+class DomesticMelonOrder:
+    """A melon order within the USA."""
+    order_type = "domestic"
+    tax = 0.08
+
+    # def __init__(self, species, qty):
+    #     """Initialize melon order attributes."""
+
+    #     self.species = species
+    #     self.qty = qty
+    #     self.shipped = False
+        # self.order_type = "domestic"
+        # self.tax = 0.08
+
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+    
+    order_type = "international"
+    tax = 0.17
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes."""
+        super().__init__(species,qty)
+        self.country_code = country_code
+
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+    
+
+# To ask at queue tomorrow:
+# Does each order mean ALL melon orders or just orders that the US government makes?
+# Does US government orders span both domestic and international orders?
+# Code review for Part 1 & 2
